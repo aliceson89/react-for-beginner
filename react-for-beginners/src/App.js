a965255638fa121ed55665dc7cb27c12b47b02b5
aliceson89 <alice.son1109@gmail.com>
2021-12-30 01:27:08 -0500

    update navbar

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
+      <Navigation />
       <Routes>
         <Route path="/movie/:id" element={<Detail />} />
         <Route path="/" element={<Home />} />
--- react-for-beginners/src/index.js
+++ react-for-beginners/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import ToDoList from "./ToDoList";
 import CoinTracker from "./CoinTracker";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
   <React.StrictMode>
